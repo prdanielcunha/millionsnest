4e4c63dffe7b8631abc3357809e92ea455d3542b
prdanielcunha <pastordanielpcunha@gmail.com>
2026-06-24 10:14:12 -0300

    feat: update dashboard mockup image
    Replace existing dashboard mockup with the updated screens asset to improve landing page visuals.

--- src/components/DashboardMockup.tsx
+++ src/components/DashboardMockup.tsx
@@ -4,7 +4,7 @@ export function DashboardMockup() {
   return (
     <div className="w-full shrink-0 flex justify-center bg-[#050505] relative select-none">
       <img 
-        src="/musicscale_tela.png" 
+        src="/telas.png" 
         alt="Dashboard Mockup" 
         className="w-full h-auto object-contain"
       />
